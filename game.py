7dd7533a8e732dca988697de19b47646c77c75ce
NitayRe <64463811+NitayRe@users.noreply.github.com>
2020-07-21 12:57:59 +0300

    Update game.py

--- game.py
+++ game.py
@@ -10,45 +10,50 @@ stackX = 0
 stackY = 0
 for i in range(24):
 	if i<12:
-		stacks.append(pieces.DrawableStack(stackX, stackY))
+		stacks.append(pieces.Stack(stackX, stackY))
 		stackX += 65
 	elif i == 12:
 		stackX = 0
 		stackY = 800
-		stacks.append(pieces.DrawableStack(stackX,stackY))
+		stacks.append(pieces.Stack(stackX,stackY))
 		stackX += 65
 	else:
-		stacks.append(pieces.DrawableStack(stackX, stackY))
+		stacks.append(pieces.Stack(stackX, stackY))
 		stackX += 65
 
-screen.blit(background_image, [0, 0])
+
+
 
 
 def main():
 	left_mouse_down = False
 	right_mouse_down = False
+	middle_mouse_down = False
 	while True:
 		for event in pg.event.get():
 			if event.type == pg.MOUSEBUTTONDOWN:
 				if event.button == 1:
 					left_mouse_down = True
+				if event.button == 2:
+					middle_mouse_down = True
 				if event.button == 3:
 					right_mouse_down = True
 
 			if event.type == pg.QUIT:
 				exit(1)
 
+			x = pg.mouse.get_pos()[0]
+			y = pg.mouse.get_pos()[1]
+			index = whichStack(x, y)
+			
 			if left_mouse_down:
-				x = pg.mouse.get_pos()[0]
-				y = pg.mouse.get_pos()[1]
-				index = whichStack(x, y)
 				stacks[index].push(pieces.Piece(pieces.Piece.BLACK))
 
 			if right_mouse_down:
-				x = pg.mouse.get_pos()[0]
-				y = pg.mouse.get_pos()[1]
-				index = whichStack(x, y)
 				stacks[index].push(pieces.Piece(pieces.Piece.WHITE))
+
+			if middle_mouse_down and not stacks[index].isEmpty():
+				stacks[index].pop()
 
 			if event.type == pg.KEYDOWN:
 				if event.key == pg.K_SPACE:
@@ -56,6 +61,9 @@ def main():
 
 			left_mouse_down = False
 			right_mouse_down = False
+			middle_mouse_down = False
+		
+		screen.blit(background_image, [0, 0])
 		for s in stacks:
 			s.draw(screen)
 		pg.display.flip()
