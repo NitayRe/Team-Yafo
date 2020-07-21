11e7751c3e5dc47e7c87c14315f37e796933b9c9
NitayRe <64463811+NitayRe@users.noreply.github.com>
2020-07-21 15:40:50 +0300

    Update logic.py

--- logic.py
+++ logic.py
@@ -19,43 +19,47 @@ def move(src, dest, stacks, removed):
 def islegal(stacks, src, dest, possibleSteps, turn):
 	if stacks[src].isEmpty() or stacks[src].getColor() != turn:
 		return False
-	if dest-src not in possibleSteps or stacks[src].isEmpty():
+	sign = -1
+	if (turn == BLACK): sign = 1
+	if sign*(dest-src) not in possibleSteps or stacks[src].isEmpty():
 		return False
+	
 	emptyColomn = stacks[dest].isEmpty()
 	sameColor = stacks[dest].getColor() == stacks[src].getColor()
-	eatable = (stacks[dest].getColor() != stacks[dest].getcolor()) and len(stacks[dest]) == 1)
+	eatable = (not sameColor) and len(stacks[dest]) == 1
 	available = emptyColomn or sameColor or eatable
 	if not available:
 		return False
 	return True
 
 
-
+clock = pg.time.Clock()
 def playOneTurn(stacks, dice1, dice2, turn, removed):
 
 	possibleSteps = [dice1, dice2]
 	if (dice1 == dice2):
-		possibleSteps.append(dice1)
-		possibleSteps.apppend(dice2)
+		possibleSteps *= 2
 
 
 	while len(possibleSteps) != 0:
 		coord=[]
 		while len(coord)<=2:
-			left_mouse_down = False
+			mousePressed = False
 			while True:
 				for event in pg.event.get():
 					if event.type == pg.MOUSEBUTTONDOWN:
 						if event.button == 1:
-							left_mouse_down = True
+							mousePressed = True
 					if event.type == pg.QUIT:
 						exit(1)
-					x = pg.mouse.get_pos()[0]
-					y = pg.mouse.get_pos()[1]
-					index = game.whichStack(x, y)
-					if left_mouse_down:
+					if mousePressed:
+						x = pg.mouse.get_pos()[0]
+						y = pg.mouse.get_pos()[1]
+						index = game.whichStack(x, y)						
 						coord.append(index)
-					left_mouse_down = False
+						clock.tick(300)
+					mousePressed = False
+					clock.tick(30)
 		src = coord[0]
 		dest = coord[1]
 		if islegal(stacks, src, dest, possibleSteps, turn):
